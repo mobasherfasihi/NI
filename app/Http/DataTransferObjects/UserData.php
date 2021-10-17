aa2e5cf49b5d987813b89354fd475fbdd0a819c2
Mobasher Fasihi <naweedmobasher@gmail.com>
2021-10-17 21:44:36 +0430

    Add feature tests for login request

---
+++ app/Http/DataTransferObjects/UserData.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Carbon;
+
+class UserData
+{
+    public function __construct(
+        public string $name,
+        public string $email,
+        public ?Carbon $email_verified_at,
+        private string $password,
+        private string $remember_token,
+        public Carbon $created_at,
+        public Carbon $updated_at,
+    )
+    {}
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use UserData;
 
 class UserFactory extends Factory
 {
@@ -20,13 +21,13 @@ class UserFactory extends Factory
      *
      * @return array
      */
-    public function definition()
+    public function definition(): array
     {
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'), // password
             'remember_token' => Str::random(10),
         ];
     }
